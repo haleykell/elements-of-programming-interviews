6a34c7dce9bef14ba7039778e5dde0fc789467d9
Haley Kell <haley.a.kell@gmail.com>
2020-08-11 23:14:21 -0500

    Convert bases
    This commit implements 7.2, base conversion.

--- strings/src/main/java/BaseConversion.java
+++ strings/src/main/java/BaseConversion.java
@@ -5,7 +5,22 @@ public class BaseConversion {
     */
 
     public static String baseConversion(String s, int b1, int b2) {
+        char[] st = s.charAt(0) == '-' ? s.substring(1).toCharArray() : s.toCharArray();
+        int sum = 0;
 
-        return "";
+        for (char c : st) {
+            sum *= b1;
+            sum += Character.isDigit(c) ? c - '0' : c - 'A' + 10;
+        }
+
+        String result = s.charAt(0) == '-' ? "-" : "";
+        result += convertBase(sum, b2);
+
+        return result;
+    }
+
+    public static String convertBase(int sum, int b2) {
+        int current = sum % b2;
+        return sum == 0 ? "" : convertBase(sum / b2, b2) + (char) (current > 9 ? 'A' + current - 10 : current + '0');
     }
 }
