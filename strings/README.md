5af0d7da98d98a4ccc7b7d9d87d5ff93a66797b0
Haley Kell <haley.a.kell@gmail.com>
2020-08-11 23:56:50 -0500

    Decode spreadsheet column
    This commit implements 7.3, decode spreadsheet column.

--- strings/README.md
+++ strings/README.md
@@ -2,7 +2,7 @@
 
 - [X] 7.1 InterconvertStringAndInteger
 - [X] 7.2 BaseConversion
-- [ ] 7.3 SpreadsheetColumnEncoding
+- [X] 7.3 SpreadsheetColumnEncoding
 - [ ] 7.4 ReplaceAndRemove
 - [ ] 7.5 PalindromeAlphanumeric
 - [ ] 7.6 ReverseWordsInASentence
--- strings/src/main/java/SpreadsheetColumnEncoding.java
+++ strings/src/main/java/SpreadsheetColumnEncoding.java
@@ -5,8 +5,13 @@ public class SpreadsheetColumnEncoding {
     */
 
     public static int decodeSpreadsheetColumn(String code) {
+        int result = 1;
 
-        return 0;
+        for (char c : code.toCharArray()) {
+            result *= (int) c - 'A' + 1;
+        }
+
+        return code.length() == 1 ? result : result + 26;
     }
 
 
